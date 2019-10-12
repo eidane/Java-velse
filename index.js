87e521cd67737543e4bb7ce961a87ff20a0c217c
eivind <eivinddyrlie1@gmail.com>
2019-10-12 14:53:58 +0200

    first save

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 function main()
 {
+	var select, i,option; 
+	//imput kilde
+	options=["Option 1", "Option 2", "Option 3", "Option 4"];
+	i=options.length;
+	console.log(i);
+	console.log(options);
 
+	select = document.getElementById('select')
+	for (x = 0; x<i; x++)
+	{
+		var opt=document.createElement('option');
+		opt.value=i
+		opt.innerHTML = options[x];
+		select.appendChild(opt);
+		console.log(i);
+	}
 }
+
